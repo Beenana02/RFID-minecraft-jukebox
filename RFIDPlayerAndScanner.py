abfed4669a4589ef74409ec9167fec21964c2d3a
Beenana02 <ghabeenana@gmail.com>
2024-07-30 17:32:45 -0400

    Updated music player script

--- RFIDPlayerAndScanner.py
+++ RFIDPlayerAndScanner.py
@@ -2,15 +2,16 @@
 
 #-Beenana02
 
-import RPi.GPIO as GPIO
-GPIO.setwarnings(False)
 from mfrc522 import SimpleMFRC522
 import pygame
 from time import sleep
 from gpiozero import Button, RotaryEncoder
-from signal import signal, SIGTERM, SIGHUP, pause
-from rpi_lcd import LCD
+#plan to add an lcd screen in later
+#from rpi_lcd import LCD
+
+#CHANGE THIS TO YOUR FOLDER WITH MUSIC#
 path = "/home/gabi/FinalRFID/music/"
+
 #List of RFID card ids
 music_list = [907369626972,390225684907,429139686602]
 reader=SimpleMFRC522()
@@ -46,24 +47,20 @@ try:
      
     #loops over song list as long as bluetooth and audio jack aren't being used 
     while (bluetoothC or auxC ==False):
-        
-        
-            
-        #signal(SIGTERM, safe_exit)
-        #signal(SIGHUP, safe_exit)
+
         print("Waiting for record scan...")
         id= reader.read()[0]
         print("Card Value is:",id) 
         if id in music_list:
             if(id == currentSong) and (pygame.mixer.music.get_busy()):
-                print('hi')
+                print('song is already playing')
             elif(paused==False):
                 if (id==907369626972):
                     
                     # playing a song
-                    import pygame
                     pygame.mixer.init()
                     pygame.mixer.music.load(path + "Ward.mp3")
+                    print('ward')
                     pygame.mixer.music.set_volume(1.0)
                     pygame.mixer.music.play()
                     currentSong=907369626972            
@@ -71,6 +68,7 @@ try:
                 
                     pygame.mixer.init()
                     pygame.mixer.music.load(path + "Stal.mp3")
+                    print('stal')
                     currentSong=390225684907
                     pygame.mixer.music.set_volume(1)
                     pygame.mixer.music.play()
@@ -86,12 +84,14 @@ try:
                     import pygame
                     pygame.mixer.init()
                     pygame.mixer.music.load(path + "Wait.mp4")
+                    print('wait')
                     currentSong=1046718834915
                     pygame.mixer.music.set_volume(1.0)
                     pygame.mixer.music.play()
 
         else:
             print('not readable disc')
+            print('playing the default mineraft soundtrack')
             pygame.mixer.init()
             pygame.mixer.music.load(path + "alpha.mp3")
             pygame.mixer.music.set_volume(1.0)
