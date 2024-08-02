f3338c57e1272cbb50811532b1bd9e3ace3bbf84
Beenana02 <ghabeenana@gmail.com>
2024-08-02 14:59:57 -0400

    Add files via upload

--- RFIDPlayerAndScanner.py
+++ RFIDPlayerAndScanner.py
@@ -1,12 +1,13 @@
-#RFID Scanner
+#RFID Jukebox
 
-#-Beenana02
-
+#-Beenana02-#
 from mfrc522 import SimpleMFRC522
 import pygame
 from time import sleep
 from gpiozero import Button, RotaryEncoder
+from subprocess import check_call
 pygame.mixer.init()
+
 #plan to add an lcd screen in later
 #from rpi_lcd import LCD
 
@@ -17,9 +18,9 @@ path = "/home/gabi/FinalRFID/music/"
 music_list = [907369626972,390225684907,429139686602]
 reader=SimpleMFRC522()
 
-#button control
+#button control center
 paused=False
-button=Button(22)
+button=Button(22, bounce_time = 1)
 def pausing1():
     global paused
     if paused:
@@ -28,24 +29,19 @@ def pausing1():
         pygame.mixer.music.unpause()
     paused = not paused
     print(paused)
-def test():
-    print("hi")
-#mute=Button(16)
-#will add volume and reset buttons/slider in the future
-#volume
+
 volumeRotary = RotaryEncoder(18,17)
 vol= 1
 pygame.mixer.music.set_volume(vol)
 def volumeCon():
     global vol
-    vol += 0.2
+    vol += 0.1
     pygame.mixer.music.set_volume(vol)
-    print(pygame.mixer.music.get_volume())
 def volumeConNega():
     global vol
-    vol -= 0.2
+    vol -= 0.1
     pygame.mixer.music.set_volume(vol)
-    print(pygame.mixer.music.get_volume())
+    
 muteB= Button(27)
 muted = False
 def muted():
@@ -55,77 +51,77 @@ def muted():
     else:
         pygame.mixer.music.set_volume(vol)
     muted = not muted
-    
+
+powerOffB = Button(24, hold_time=5)
+def shutDown():
+    check_call(['sudo','poweroff'])
+
+def buttonController():
+    powerOffB.when_held = shutDown
+    if pygame.mixer.music.get_busy():
+        button.when_pressed = pausing1
+        muteB.when_pressed = muted
+        volumeRotary.when_rotated_clockwise = volumeCon
+        volumeRotary.when_rotated_counter_clockwise = volumeConNega    
+
 
 #Shows what songs is currently playing to prevent a double reading 
 currentSong=0
 
 bluetoothC=False
 auxC=False
-
-def safe_exit(signum, frame):
-    exit(1)
- 
-try:
-    button.when_pressed = pausing1
-    muteB.when_pressed = muted
-    sleep(0.2)
-    volumeRotary.when_rotated_clockwise = volumeCon
-    volumeRotary.when_rotated_counter_clockwise = volumeConNega
-    
-     
-    #loops over song list as long as bluetooth and audio jack aren't being used 
-    while (bluetoothC or auxC ==False):
-
-        print("Waiting for record scan...")
-        id= reader.read()[0]
-        print("Card Value is:",id) 
-        if id in music_list:
-            if(id == currentSong) and (pygame.mixer.music.get_busy()):
-                print('song is already playing')
-            elif(paused==False):
-                if (id==907369626972):
-                    
-                    # playing a song
-                    pygame.mixer.init()
-                    pygame.mixer.music.load(path + "Ward.mp3")
-                    print('ward')
-                    pygame.mixer.music.set_volume(1.0)
-                    pygame.mixer.music.play()
-                    currentSong=907369626972            
-                elif (id==390225684907):
+      
+#loops over song list as long as bluetooth and audio jack aren't being used     
+while (bluetoothC or auxC ==False):
+    buttonController()
+    print("Waiting for record scan...")
+    id= reader.read()[0]
+    print("Card Value is:",id) 
+    if id in music_list:
+        if(id == currentSong) and (pygame.mixer.music.get_busy()):
+            print('song is already playing')
+        elif(id != currentSong):
+            if (id==907369626972):
+                pygame.mixer.init()
+                pygame.mixer.music.load(path + "Ward.mp4")
+                print('ward')
+                pygame.mixer.music.set_volume(1.0)
+                pygame.mixer.music.play()
+                currentSong=907369626972
                 
-                    pygame.mixer.init()
-                    pygame.mixer.music.load(path + "Stal.mp3")
-                    print('stal')
-                    currentSong=390225684907
-                    pygame.mixer.music.set_volume(1)
-                    pygame.mixer.music.play()
-                elif (id==429139686602):
+            elif (id==390225684907):
+                pygame.mixer.init()
+                pygame.mixer.music.load(path + "Stal.mp3")
+                print('stal')
+                currentSong=390225684907
+                pygame.mixer.music.set_volume(1)
+                pygame.mixer.music.play()
                 
-                    pygame.mixer.init()
-                    pygame.mixer.music.load(path + "pigstep.mp3")
-                    print('pigstep')
-                    currentSong=429139686602
-                    pygame.mixer.music.set_volume(1.0)
-                    pygame.mixer.music.play()
-                elif (id==1046718834915):
-                    import pygame
-                    pygame.mixer.init()
-                    pygame.mixer.music.load(path + "Wait.mp4")
-                    print('wait')
-                    currentSong=1046718834915
-                    pygame.mixer.music.set_volume(1.0)
-                    pygame.mixer.music.play()
+            elif (id==429139686602):
+                pygame.mixer.init()
+                pygame.mixer.music.load(path + "Pigstep.mp4")
+                print('pigstep')
+                currentSong=429139686602
+                pygame.mixer.music.set_volume(1.0)
+                pygame.mixer.music.play()
+                
+            elif (id==1046718834915):
+                import pygame
+                pygame.mixer.init()
+                pygame.mixer.music.load(path + "Wait.mp4")
+                print('wait')
+                currentSong=1046718834915
+                pygame.mixer.music.set_volume(1.0)
+                pygame.mixer.music.play()
 
-        else:
-            print('not readable disc')
-            print('playing the default mineraft soundtrack')
-            pygame.mixer.init()
-            pygame.mixer.music.load(path + "alpha.mp3")
-            pygame.mixer.music.set_volume(1.0)
-            pygame.mixer.music.play()
-            currentSong=None
+    else:
+        print('not readable disc')
+        print('playing the default mineraft soundtrack')
+        pygame.mixer.init()
+        pygame.mixer.music.load(path + "alpha.mp3")
+        pygame.mixer.music.set_volume(1.0)
+        pygame.mixer.music.play()
+        currentSong=None
+        
 
-finally:
-    pass
+
