4068e86b6ecbd696507ef2d67f0765615013d511
Beenana02 <ghabeenana@gmail.com>
2024-08-26 20:32:31 -0400

    Update resetMusic.py Still WIP

--- resetMusic.py
+++ resetMusic.py
@@ -1,18 +1,28 @@
 #Beenana02#
 
 #script is meant to reset the jukebox script#
+import RPi.GPIO as GPIO
 from gpiozero import Button
 import time
 import os, signal
 import subprocess
-
 resetB= Button(23, bounce_time=1)
-scriptOn= True
+scriptOn= False
 def onOff():
+    global subprocess
+    extProc = subprocess.Popen(['python','/home/gabi/FinalRFID/RFIDPlayerAndScanner.py'])
+    global scriptOn
+    
     if scriptOn == True:
-        os.killpg('/home/gabi/FinalRFID/RFIDPlayerAndScanner.py'.pid, signal.SIGTERM)
+        print('off')
+        subprocess.Popen.terminate(extProc)
+        scriptOn = False
     else:
-        subprocess.run('/home/gabi/FinalRFID/RFIDPlayerAndScanner.py')
+        subprocess.Popen('python /home/gabi/FinalRFID/RFIDPlayerAndScanner.py',shell=True, preexec_fn=os.setsid)
+        scriptOn = True
 
 while True:
-    resetB.when_pressed = onOff
+    try:
+        resetB.when_pressed = onOff
+    except KeyboardInterrupt:
+        GPIO.cleanup() 
